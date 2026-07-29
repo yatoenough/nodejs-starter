bd1c3bad4bb89ff8c6331c21736463d7299621ef
Nikita Shyshkin <yatoenough@gmail.com>
2026-07-30 00:41:22 +0200

    optimize scripts

--- src/user.ts
+++ src/user.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-	id: z.number(),
-	name: z.string(),
-	email: z.email(),
+  id: z.number(),
+  name: z.string(),
+  email: z.email(),
 });
 
 export type User = z.infer<typeof UserSchema>;
